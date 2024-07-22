52a4b2558afaa6210b0c2a3c2078f167f33a1c0d
Saleher Begum <58544434+Saleher@users.noreply.github.com>
2024-07-22 07:39:28 +0000

    push 2

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+/*
+Primitive data types
+
+01. Number
+02. String
+03. Boolean
+04. Undefined => Value not assigned yet
+05. Null => Its an stand alone value and treats as and Object in Javascript
+06. Bigint
+07. Symbol => use to find uniqueness 
+*/
+
+
+
+
+// Object
+
+console.log(typeof undefined)
+console.log(typeof null)
