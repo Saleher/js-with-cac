b79c75ee8ca5a42b88ec308b182620b2bcb8d908
Saleher Begum <58544434+Saleher@users.noreply.github.com>
2024-07-23 14:57:41 +0000

    array cont...

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,4 +1,31 @@
 // Dates
 
-let myDate = new Date();
-console.log(myDate);
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(typeof myDate) // object
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreateDate = new Date("2023-01-14");
+// console.log(myCreateDate.toLocaleDateString());
+
+// let myCreatedDate = new Date("01-14-2025");
+
+
+// let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(Date.now().getTime());
+
+// console.log(Math.floor(Date.now() / 10000));
+
+let newDate = new Date();
+console.log(newDate.getMonth());
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleDateString('default', {
+    weekday: "long"
+});
+
+
+
---
+++ 02_basics/01_array.js
@@ -0,0 +1,49 @@
+// array
+const myArray = [0, 1, 2, 3, 4];
+// console.log(myArray[0]);
+
+
+// const myHeros = ["APJ", "Bhagat Singh", "Netaji"];
+
+// const myArr2 = new Array(1, 2, 3, 4);
+
+
+// Array methods
+// myArray.push(6);   
+// myArray.pop(); // remove last element
+// console.log(myArray);
+
+
+// myArray.unshift(9); // add 9 at the begining of array
+// myArray.shift(); // remove first element
+// console.log(myArray.includes(9));  // return true or false
+// myArray.log(myArray.indexOf(3));  // return -1 if value is not available
+
+
+// const newArr = myArray.join();  // join arrays and convert into string
+
+
+
+
+// console.log(myArray);
+// console.log(newArr);
+
+
+// slice , splice
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1, 3);
+
+console.log(myn1);
+
+
+
+
+
+
+
+
+
+
+
