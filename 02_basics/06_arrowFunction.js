5df4d2bf07452433a8cccd7d090267c6290783f2
Saleher Begum <58544434+Saleher@users.noreply.github.com>
2024-07-26 11:40:57 +0000

    arrowFun end

--- 02_basics/06_arrowFunction.js
+++ 02_basics/06_arrowFunction.js
@@ -5,19 +5,39 @@ const user = {
         console.log(`Hello ${this.username}`)
         console.log(this);
     }
-
-    
-
-
 }
 
 // user.welcomeMsg();
 // user.username = "Begum";
 
-user.welcomeMsg();
-console.log(`From outer: ${this}`);
+// user.welcomeMsg();
+// console.log(`From outer: ${this}`);
 
 
-const addTwo = () => {
-    
-}
+// const addTwo = (num1, num2) => ({username: "saleher"});
+
+// console.log(addTwo(3, 4));
+
+
+
+// IIFE Immediately Invoked Function Expression 
+// The function that execute immidiatly after 
+// We use IIFE for avoid global plution
+
+// (function myIfe(){
+//     console.log(`DB Connected`);
+// })();
+
+
+
+
+
+(function myFun(){
+    // Named IIFE
+    console.log(`DB Connected`);
+})();
+
+
+((name) => {
+    console.log(`DB Connected 2 ${name}`);
+})("Name here");
