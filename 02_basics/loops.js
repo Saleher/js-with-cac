0937c389947c5b687d4eacce0a850ff46721f90a
Saleher Begum <58544434+Saleher@users.noreply.github.com>
2024-07-30 10:23:52 +0000

    reduced

--- 02_basics/loops.js
+++ 02_basics/loops.js
@@ -88,8 +88,16 @@ const myCoading = [
 
 const myNumbers = [1,2,3,4,5,6,7,8,9,10];
 
-const newNums = myNumbers
-.map((num) => num * 10)
-.map((num) => num + 1)
-.filter((num) => num >= 40);
-console.log(newNums);
+// const newNums = myNumbers
+// .map((num) => num * 10)
+// .map((num) => num + 1)
+// .filter((num) => num >= 40);
+// console.log(newNums);
+
+
+const myTotal = myNumbers.reduce(function(acc, currval){
+    console.log(`acc: ${acc} and currval: ${currval}`)
+    return acc + currval;
+}, 0)
+
+console.log(myTotal);
