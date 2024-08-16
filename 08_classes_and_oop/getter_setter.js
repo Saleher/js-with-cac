7625c3ae56f6afc56c09c679d0869aaf61080667
Saleher Begum <58544434+Saleher@users.noreply.github.com>
2024-08-16 06:57:01 +0000

    geter setter

---
+++ 08_classes_and_oop/getter_setter.js
@@ -0,0 +1,29 @@
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email = value;
+    }
+
+
+    get password(){
+        return this._password.toUpperCase();
+    }
+
+    set password(value){
+        this._password = value;
+    }
+
+}
+
+
+const hitesh = new User("h@hitesh.com", "abcd");
+console.log(`${hitesh.password} : ${hitesh.email}`)
